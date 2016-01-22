927430f65b0e2bb9d9fef626f02ec0e3a756d9a6
Cosmo <cosmo@cosmointeractive.co>
2016-01-22 02:22:33 -0500

    More changes to grid fields

--- application/modules/content/content_fields/views/settings/grid.php
+++ application/modules/content/content_fields/views/settings/grid.php
@@ -7,6 +7,17 @@
 </div>
 
 <div>
+    <label for="type">Output Type:</label>
+    <?php echo form_dropdown(
+        'settings[output]', 
+        array('image'  => 'Image', 'text_field' => 'Text Field',), 
+        set_value('settings[output]', 
+        ( ! empty($Field->settings['output'])) ? $Field->settings['output'] : ''), 
+        'id="output_type"'
+    ); ?>
+</div>
+
+<div class="grid_setting">
     <label for="type">Max Rows:</label>
     <?php echo form_input(array(
         'name'  => 'settings[max_rows]', 
@@ -14,3 +25,17 @@
         'value' => set_value('settings[max_rows]', ( ! empty($Field->settings['max_rows'])) ? $Field->settings['max_rows'] : '')
     )); ?>
 </div>
+
+<script type="text/javascript">
+    $(document).ready( function() {
+        $('#output_type').change( function() {
+            if ($(this).val() == 'image') {
+                $('.grid_setting').show();
+            } else {
+                $('.grid_setting').hide();
+            }
+        });
+
+        $('#output_type').trigger('change');
+    });
+</script>
