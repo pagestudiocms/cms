bb3f7489a44969fec5677a793cb7a899340190a2
Cosmo <cosmo@cosmointeractive.co>
2016-08-10 13:29:23 -0400

    Updated sitemap controller

--- application/modules/sitemap/controllers/sitemap.php
+++ application/modules/sitemap/controllers/sitemap.php
@@ -33,7 +33,7 @@
  * @category	Common Functions
  * @author  	Cosmo Mathieu <cosmo@cosmointeractive.co>
  * @link		http://pagestudiocms.com/docs/
- * @version 	1.0
+ * @version 	1.1
  * @since       Version 1.2.0
  * @uses        https://github.com/chemicaloliver/codeigniter-sitemaps
  */
@@ -61,7 +61,10 @@ class Sitemap extends Public_Controller
     {        
         $this->load->library('sitemaps');
         $this->load->model('sitemap_model');
-
+        
+        // Override default required fields
+        $this->sitemaps->set_item_keys(['loc', 'lastmod']);
+        
         // First get a list of published entries and their urls 
         $posts = $this->sitemap_model->get_entries();       
 
@@ -70,8 +73,9 @@ class Sitemap extends Public_Controller
             $item = array(
                 "loc" => site_url($post->slug),
                 "lastmod" => date("c", strtotime($post->modified_date)),
-                "changefreq" => "hourly",
-                "priority" => "0.8"
+                // **CHANGED** 20160810 - Not sure these are helping at all...
+                // "changefreq" => "monthly",
+                // "priority" => "0.8"
             );
 
             $this->sitemaps->add_item($item);
@@ -79,8 +83,14 @@ class Sitemap extends Public_Controller
 
         // Save the file to the root dir of the site
         // File name may change when compression is enabled
-        $file_name = $this->sitemaps->build("sitemap.xml", false);
-
-        $reponses = $this->sitemaps->ping(site_url($file_name));        
+        // $file_name = $this->sitemaps->build("sitemap.xml", false);
+        $file_name = $this->sitemaps->build(null, false);
+        
+        // Notify search engines of our changes 
+        $reponses = $this->sitemaps->ping(site_url($file_name));
+        
+        header ("Content-Type:text/xml");
+        print($file_name);
+        exit;
     }
 }
